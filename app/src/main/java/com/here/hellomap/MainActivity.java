6710b9759bbc0fcf23d990f780d0116cad33134c
Levi <levicrietee@hotmail.com>
2020-12-07 16:23:25 +0100

    add project

---
+++ app/src/main/java/com/here/hellomap/MainActivity.java
@@ -0,0 +1,167 @@
+/*
+ * Copyright (C) 2019-2020 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.hellomap;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.here.hellomap.PermissionsRequestor.ResultListener;
+import com.here.sdk.core.Anchor2D;
+import com.here.sdk.core.GeoCoordinates;
+import com.here.sdk.core.Point2D;
+import com.here.sdk.gestures.TapListener;
+import com.here.sdk.mapview.MapError;
+import com.here.sdk.mapview.MapImage;
+import com.here.sdk.mapview.MapImageFactory;
+import com.here.sdk.mapview.MapMarker;
+import com.here.sdk.mapview.MapScene;
+import com.here.sdk.mapview.MapScheme;
+import com.here.sdk.mapview.MapView;
+import com.here.sdk.mapview.MapViewBase;
+import com.here.sdk.mapview.PickMapItemsResult;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private PermissionsRequestor permissionsRequestor;
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get a MapView instance from the layout.
+        mapView = findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+
+        mapView.setOnReadyListener(new MapView.OnReadyListener() {
+            @Override
+            public void onMapViewReady() {
+                // This will be called each time after this activity is resumed.
+                // It will not be called before the first map scene was loaded.
+                // Any code that requires map data may not work as expected beforehand.
+                Log.d(TAG, "HERE Rendering Engine attached.");
+                addMapMarker();
+            }
+        });
+
+        handleAndroidPermissions();
+    }
+
+    private void handleAndroidPermissions() {
+        permissionsRequestor = new PermissionsRequestor(this);
+        permissionsRequestor.request(new ResultListener(){
+
+            @Override
+            public void permissionsGranted() {
+                loadMapScene();
+                setTapGestureHandler();
+            }
+
+            @Override
+            public void permissionsDenied() {
+                Log.e(TAG, "Permissions denied by user.");
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsRequestor.onRequestPermissionsResult(requestCode, grantResults);
+    }
+
+    private void loadMapScene() {
+        // Load a scene from the HERE SDK to render the map with a map scheme.
+        mapView.getMapScene().loadScene(MapScheme.NORMAL_DAY, new MapScene.LoadSceneCallback() {
+            @Override
+            public void onLoadScene(@Nullable MapError mapError) {
+                if (mapError == null) {
+                    double distanceInMeters = 1000 * 10;
+                    mapView.getCamera().lookAt(
+                            new GeoCoordinates(51.44416,5.4788), distanceInMeters);
+                } else {
+                    Log.d(TAG, "Loading map failed: mapError: " + mapError.name());
+                }
+            }
+        });
+    }
+
+    public void addMapMarker(){
+        GeoCoordinates geoCoordinates = new GeoCoordinates(51.44416,5.4788);
+        Anchor2D anchor2D = new Anchor2D(0.5f, 1.0f);
+        MapImage mapImage = MapImageFactory.fromResource(this.getResources(), R.drawable.ad_picture);
+        MapMarker mapMarker = new MapMarker(geoCoordinates, mapImage, anchor2D);
+        mapView.getMapScene().addMapMarker(mapMarker);
+    }
+
+    private void setTapGestureHandler() {
+        mapView.getGestures().setTapListener(new TapListener() {
+            @Override
+            public void onTap(Point2D touchPoint) {
+                pickMapMarker(touchPoint);
+            }
+        });
+    }
+
+    private void pickMapMarker(final Point2D touchPoint) {
+        float radiusInPixel = 2;
+        mapView.pickMapItems(touchPoint, radiusInPixel, new MapViewBase.PickMapItemsCallback() {
+            @Override
+            public void onPickMapItems(@NonNull PickMapItemsResult pickMapItemsResult) {
+                List<MapMarker> mapMarkerList = pickMapItemsResult.getMarkers();
+                if (mapMarkerList.size() == 0) {
+                    return;
+                }
+                System.out.println("Works levi!");
+                MapMarker topmostMapMarker = mapMarkerList.get(0);
+                String text = "Map marker picked: Location: " +
+                        topmostMapMarker.getCoordinates().latitude + ", " +
+                        topmostMapMarker.getCoordinates().longitude;
+                Toast.makeText(getApplicationContext(),text ,
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+}
